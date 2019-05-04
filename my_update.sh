229cb4b4fdca66c59c9a56cc47d19da7711bb6c3
Lynx <1197225628@qq.com>
2019-05-04 10:57:02 +0800

    x

--- my_update.sh
+++ my_update.sh
@@ -2,6 +2,12 @@ if [ "$1"x = x ]
 then
   echo "\033[1;31mneed message\033[0m"
 else
+  # 切换目录
+  cd "./source"
+  echo "\033[1;33m[\033[0m"
+  echo $PWD
+  echo "\033[1;33m]\033[0m"
+
   echo "\033[1;32mAdding...\033[0m"
   git add -A
   git commit -m $1
@@ -10,7 +16,13 @@ else
   then
     sleep 1
     echo "\033[1;33mBut you don't have any branch\033[0m"
+    echo "\033[1;33m[\033[0m"
+    git remote "-v"
+    git ls-files
+    echo "\033[1;33m]\033[0m"
   else
     echo "\033[1;35mAbort\033[0m"
   fi
+  cd ".."
 fi
+
